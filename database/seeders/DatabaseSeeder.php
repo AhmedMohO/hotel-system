40bfe33ea5baceb00ce5ea5cb99d2e62b1822717
Moahmed <redarakha@std.mans.edu.eg>
2026-03-25 22:56:01 +0200

    api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             ManagerSeeder::class,
             ReceptionistSeeder::class,
+            RoomSeeder::class,
         ]);
     }
 }
